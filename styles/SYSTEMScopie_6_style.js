8a7b1a90d55a32d9a6092f497f9dc57db4eb36b5
Hugo <hugoregad@gmail.com>
2024-05-03 21:40:55 +0200

    Add files via upload

--- styles/SYSTEMScopie_6_style.js
+++ styles/SYSTEMScopie_6_style.js
@@ -7,7 +7,8 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
         variables: {}
     };
     var value = '';
-    var labelText = "";
+    var labelText_up = "";
+    var labelText_dw = "";
     size = 0;
     var labelFont = "11.700000000000001px \'Franklin Gothic Medium\', sans-serif";
     var labelFill = "#484848";
@@ -18,7 +19,10 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
     var offsetY = 3;
     var placement = 'point';
     if (feature.get("NAME") !== null) {
-        labelText = String(feature.get("NAME"));
+        labelText_up = String(feature.get("NAME")).toUpperCase();
+    }
+    if (feature.get("NAME") !== null) {
+        labelText_dw = String(feature.get("NAME"));
     }
     
         function rules_SYSTEMScopie_6(feature, value) {
@@ -27,15 +31,26 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                 variables: {}
             };
             // Start of if blocks and style check logic
-            if (exp_SYSTEMScopie_6rule0_eval_expression(context)) {
+            if (exp_SYSTEMScopie_6rule0_eval_expression(context) && resolution < 18) {
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 10.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(209,10,13,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
+        text: createTextStyle(feature, resolution, labelText_up, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
-                    }
+            }
+                    if (exp_SYSTEMScopie_6rule0_eval_expression(context) && resolution >= 18) {
+                      return [new ol.style.Style({
+        image: new ol.style.Circle({
+                        radius: 10.0 + size,
+                        stroke: new ol.style.Stroke({ color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52 }), fill: new ol.style.Fill({ color: 'rgba(209,10,13,1.0)' })
+                    }),
+        text: createTextStyle(feature, resolution, labelText_up, labelFont,
+                        labelFill, placement, bufferColor,
+                        bufferWidth)
+                })];
+            }
                     else if (exp_SYSTEMScopie_6rule1_eval_expression(context)) {
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 8.0 + size,
@@ -79,7 +94,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                               bufferWidth)
     })];
                     }
-                    else if (exp_SYSTEMScopie_6rule5_eval_expression(context)) {
+                    else if (exp_SYSTEMScopie_6rule5_eval_expression(context) ) {
                       return [ new ol.style.Style({
         image: new ol.style.RegularShape({radius: 2.4 + size, points: 4,
             angle: Math.PI/4, stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(119,119,119,1.0)'})}),
