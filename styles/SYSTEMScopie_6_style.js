85eac59aaa59f20994dca806e9482225c3b17c4c
Hugo <hugoregad@gmail.com>
2024-05-03 22:30:23 +0200

    Add files via upload

--- styles/SYSTEMScopie_6_style.js
+++ styles/SYSTEMScopie_6_style.js
@@ -7,7 +7,6 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
         variables: {}
     };
     var value = '';
-    var labelText = "";
     size = 0;
     var labelFont = "11.700000000000001px \'Franklin Gothic Medium\', sans-serif";
     var labelFill = "#484848";
@@ -18,49 +17,64 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
     var offsetY = 3;
     var placement = 'point';
     if (feature.get("NAME") !== null) {
-        labelText = String(feature.get("NAME"));
+       labelText = String(feature.get("NAME"));
     }
-    
+      
         function rules_SYSTEMScopie_6(feature, value) {
             var context = {
                 feature: feature,
                 variables: {}
             };
             // Start of if blocks and style check logic
-            if (exp_SYSTEMScopie_6rule0_eval_expression(context)) {
+            if (exp_SYSTEMScopie_6rule0_eval_expression(context) && resolution < 18) {
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 10.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(209,10,13,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
+        text: createTextStyle(feature, resolution, labelText.toUpperCase(), labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
-                    }
+            }
+
                     else if (exp_SYSTEMScopie_6rule1_eval_expression(context)) {
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 8.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(36,36,36,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(236,228,155,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
+        text: createTextStyle(feature, resolution, labelText.toUpperCase(), labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
                     }
-                    else if (exp_SYSTEMScopie_6rule2_eval_expression(context)) {
-                      return [ new ol.style.Style({
+            else if (exp_SYSTEMScopie_6rule2_eval_expression(context) && resolution < 18) {
+                return [new ol.style.Style({
+                    image: new ol.style.Icon({
+                        imgSize: [580, 580],
+                        scale: 0.05517241379310345,
+                        anchor: [16, 16],
+                        anchorXUnits: "pixels",
+                        anchorYUnits: "pixels",
+                        rotation: 1.047198,
+                        src: "styles/amenity_firestation.svg"
+                    })
+                })];
+            }
+
+                    else if (exp_SYSTEMScopie_6rule2_eval_expression(context)) && resolution >= 18 {
+    return [new ol.style.Style({
         image: new ol.style.Icon({
-                  imgSize: [580, 580],
-                  scale: 0.05517241379310345,
-                  anchor: [16, 16],
-                  anchorXUnits: "pixels",
-                  anchorYUnits: "pixels",
-                  rotation: 1.047198,
-                  src: "styles/amenity_firestation.svg"
-            }),
+            imgSize: [580, 580],
+            scale: 0.05517241379310345,
+            anchor: [16, 16],
+            anchorXUnits: "pixels",
+            anchorYUnits: "pixels",
+            rotation: 1.047198,
+            src: "styles/amenity_firestation.svg"
+        }),
         text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
+            labelFill, placement, bufferColor,
+            bufferWidth)
     })];
-                    }
+}
                     else if (exp_SYSTEMScopie_6rule3_eval_expression(context)) {
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 5.6 + size,
@@ -79,7 +93,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                               bufferWidth)
     })];
                     }
-                    else if (exp_SYSTEMScopie_6rule5_eval_expression(context)) {
+                    else if (exp_SYSTEMScopie_6rule5_eval_expression(context) ) {
                       return [ new ol.style.Style({
         image: new ol.style.RegularShape({radius: 2.4 + size, points: 4,
             angle: Math.PI/4, stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(119,119,119,1.0)'})}),
