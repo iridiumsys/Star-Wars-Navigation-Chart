36899d4bf2c66e663e6050458cb0bf7db60f6f53
Hugo <hugoregad@gmail.com>
2024-05-03 22:03:20 +0200

    Add files via upload

--- styles/SYSTEMScopie_6_style.js
+++ styles/SYSTEMScopie_6_style.js
@@ -7,8 +7,6 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
         variables: {}
     };
     var value = '';
-    var labelText_up = String(feature.get("NAME")).toUpperCase();
-    var labelText_dw = String(feature.get("NAME"));;
     size = 0;
     var labelFont = "11.700000000000001px \'Franklin Gothic Medium\', sans-serif";
     var labelFill = "#484848";
@@ -18,13 +16,10 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
     var offsetX = 8;
     var offsetY = 3;
     var placement = 'point';
-    //if (feature.get("NAME") !== null) {
-      //  labelText_up = String(feature.get("NAME")).toUpperCase();
-    //}
-    //if (feature.get("NAME") !== null) {
-      //  labelText_dw = String(feature.get("NAME"));
-    //}
-    
+    if (feature.get("NAME") !== null) {
+       labelText = String(feature.get("NAME"));
+    }
+      
         function rules_SYSTEMScopie_6(feature, value) {
             var context = {
                 feature: feature,
@@ -35,27 +30,17 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 10.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(209,10,13,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_up, labelFont,
+        text: createTextStyle(feature, resolution, labelText.toUpperCase(), labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
             }
-                    if (exp_SYSTEMScopie_6rule0_eval_expression(context) && resolution >= 18) {
-                      return [new ol.style.Style({
-        image: new ol.style.Circle({
-                        radius: 10.0 + size,
-                        stroke: new ol.style.Stroke({ color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52 }), fill: new ol.style.Fill({ color: 'rgba(209,10,13,1.0)' })
-                    }),
-        text: createTextStyle(feature, resolution, labelText_up, labelFont,
-                        labelFill, placement, bufferColor,
-                        bufferWidth)
-                })];
-            }
+
                     else if (exp_SYSTEMScopie_6rule1_eval_expression(context)) {
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 8.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(36,36,36,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(236,228,155,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -71,7 +56,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                   rotation: 1.047198,
                   src: "styles/amenity_firestation.svg"
             }),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -80,7 +65,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 5.6 + size,
             stroke: new ol.style.Stroke({color: 'rgba(24,24,24,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(237,237,237,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -89,7 +74,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 9.6 + size,
             stroke: new ol.style.Stroke({color: 'rgba(90,90,90,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(255,255,255,0.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -98,7 +83,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.RegularShape({radius: 2.4 + size, points: 4,
             angle: Math.PI/4, stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(119,119,119,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -107,7 +92,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.RegularShape({radius: 2.4 + size, points: 4,
             angle: Math.PI/4, stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(119,119,119,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -116,7 +101,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 2.4 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(119,119,119,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -125,7 +110,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 2.4 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(119,119,119,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -134,7 +119,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 6.8 + size,
             stroke: new ol.style.Stroke({color: 'rgba(218,211,197,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 3.04}), fill: new ol.style.Fill({color: 'rgba(34,34,34,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -143,19 +128,19 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 4.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(241,241,241,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     }),new ol.style.Style({
         image: new ol.style.Circle({radius: 4.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(241,241,241,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     }),new ol.style.Style({
         image: new ol.style.Circle({radius: 4.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(241,241,241,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -164,7 +149,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.Circle({radius: 6.8 + size,
             stroke: new ol.style.Stroke({color: 'rgba(53,38,60,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(98,78,115,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
@@ -173,7 +158,7 @@ var style_SYSTEMScopie_6 = function(feature, resolution){
                       return [ new ol.style.Style({
         image: new ol.style.RegularShape({radius: 5.2 + size, points: 4,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(247,247,247,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText_dw, labelFont,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
