34c5bad57014b6a3c5a6aef2c90dd31e16d36a19
Hugo <hugoregad@gmail.com>
2024-06-02 12:08:45 +0200

    Update search.js

--- tools/search.js
+++ tools/search.js
@@ -12,20 +12,30 @@ function searchFeatureByName(name) {
             const extent = geometry.getExtent();
             map.getView().fit(extent, { duration: 1000 });
             // Set a minimum zoom level after zooming
-            map.getView().setMinZoom(20); // Example: Setting minimum zoom level to 10
+            map.getView().setMinZoom(20); // Example: Setting minimum zoom level to 20
             return;
         }
     }
     alert('Object not found');
 }
 
-// Ajouter un écouteur d'événement au bouton de recherche
-document.getElementById('search-button').addEventListener('click', function() {
+// Function to handle search
+function handleSearch() {
     const searchInput = document.getElementById('search-input').value;
     searchFeatureByName(searchInput);
     // Reset the minimum zoom level after each search
     map.getView().setMinZoom(originalMinZoom);
-        // Clear the map's view for the next search
+    // Clear the map's view for the next search
     map.getView().setCenter([0.00847486, -0.0109944]); // Reset center to initial position
     map.getView().setZoom(18); // Reset zoom level
+}
+
+// Event listener for "Enter" key press on search input field
+document.getElementById('search-input').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        handleSearch();
+    }
 });
+
+// Event listener for search button click
+document.getElementById('search-button').addEventListener('click', handleSearch);
