ec59bb18e9edb20953ad44f910b29811cda7aeab
Hugo <hugoregad@gmail.com>
2024-06-23 00:28:45 +0200

    Update tooltip.js

--- tools/tooltip.js
+++ tools/tooltip.js
@@ -3,9 +3,15 @@ var tooltip = document.createElement('div');
 tooltip.setAttribute('class', 'tooltip');
 document.body.appendChild(tooltip);
 
-// Fonction pour afficher le tooltip
-function showTooltip(event, message) {
-    tooltip.innerHTML = message;
+// Fonction pour afficher le tooltip avec le nom et l'image du système
+function showTooltip(event, properties) {
+    var imageTag = properties.picture ? `<img src="${properties.picture}" alt="System Image" class="popup-image">` : '';
+    var tooltipContent = `<div class="tooltip-content">
+                            <h3>${properties.NAME}</h3>
+                            ${imageTag}
+                          </div>`;
+    
+    tooltip.innerHTML = tooltipContent;
     tooltip.style.left = event.pageX + 'px';
     tooltip.style.top = event.pageY + 'px';
     tooltip.style.display = 'block';
