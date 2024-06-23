7f3f804dbeefa4551c6cd91b51745f682ab8727b
Hugo <hugoregad@gmail.com>
2024-06-23 09:01:19 +0200

    Update tooltip.js

--- tools/tooltip.js
+++ tools/tooltip.js
@@ -10,14 +10,26 @@ function showTooltip(event, properties) {
                             <h3>${properties.NAME}</h3>
                             ${imageTag}
                           </div>`;
-    
+
     tooltip.innerHTML = tooltipContent;
-    tooltip.style.left = event.pageX + 'px';
-    tooltip.style.top = event.pageY + 'px';
     tooltip.style.display = 'block';
+    positionTooltip(event);
+}
+
+// Fonction pour positionner le tooltip près du curseur
+function positionTooltip(event) {
+    tooltip.style.left = event.pageX + 10 + 'px'; // Décalage de 10px à droite du curseur
+    tooltip.style.top = event.pageY + 10 + 'px'; // Décalage de 10px en dessous du curseur
 }
 
 // Fonction pour cacher le tooltip
 function hideTooltip() {
     tooltip.style.display = 'none';
 }
+
+// Suivi du mouvement de la souris pour mettre à jour la position du tooltip
+document.addEventListener('mousemove', function(event) {
+    if (tooltip.style.display === 'block') {
+        positionTooltip(event);
+    }
+});
